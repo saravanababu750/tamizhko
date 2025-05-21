d956c03a5785f8de7c01218149acec72d743a332
saravanababu750 <saravanababu750@gmail.com>
2025-05-21 15:13:12 +0530

    2nd commit

--- ApiGateway/src/main/java/com/cloud/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/cloud/ApiGateway/ApiGatewayApplication.java
@@ -8,7 +8,7 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
-		System.out.println("hi");
+		System.out.println("feature3");
 	}
 
 
